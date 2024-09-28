f65429c93d7d19241ecbe6db4e22234cb055cc36
Yuki Watanabe <ukwhatn@gmail.com>
2024-09-29 00:22:31 +0900

    fin

--- create_pages.py
+++ create_pages.py
@@ -1,6 +1,5 @@
 import os
 import re
-from pprint import pprint
 
 import wikidot
 from dotenv import load_dotenv
@@ -15,6 +14,7 @@ with open("targets.txt", "r") as f:
     raw_targets = f.read().splitlines()
 
 targets = []
+sources = []
 regex = re.compile(r"^(.+)\((\d+)\)/(.+)$")
 for target in raw_targets:
     match = regex.match(target)
@@ -28,9 +28,8 @@ for target in raw_targets:
         print(f"Invalid target: {target}")
         raise ValueError
 
-
 with wikidot.Client(username=WD_USER, password=WD_PASS) as client:
-    site = client.site.get("pseudo-scp-jp")
+    site = client.site.get("scp-jp")
 
     for target in targets:
         source = f"""[[>]]
@@ -46,9 +45,33 @@ with wikidot.Client(username=WD_USER, password=WD_PASS) as client:
         title = f"恐怖コン決選投票-{target['tag']}/{target['fullname']}"
 
         page = site.page.create(
-            fullname=f"uktest:poll:{target['fullname']}",
+            fullname=f"poll:terror24-{target['fullname']}",
             title=title,
             source=source,
             comment=f"決選投票ページ作成: {target['tag']}/{target['fullname']}"
         )
 
+        page.set_tags(["jp", "投票"])
+
+        # ページ掲載用ソース生成
+        source = f"""[[module listpages category="_default" fullname="{target['fullname']}" limit="1" separate="no" wrapper="no"]]
+[[div_ class="runoff-item"]]
+[[div_ class="runoff-heading"]]
+%%title_linked%%
+[[/div]]
+[[div_ class="runoff-author"]]
+by [[user %%created_by_unix%%]]
+[[/div]]
+[[div_ class="runoff-voting"]]
+評価： [[span class="runoff-voting-value"]]%%rating%%[[/span]]
+[[/div]]
+[[div_ class="runoff-ratemodule"]]
+[[iframe http://scp-jp.wikidot.com/poll:terror24-%%fullname%%?theme_url=https://scp-jp.wdfiles.com/local--code/terror-contest-2024/1 scrolling="no" style="width:100%;height:1.75em;overflow:hidden;" frameborder="0"]]
+[[/div]]
+[[/div]]
+[[/module]]"""
+        sources.append(source)
+        print(source)
+
+with open("sources.txt", "w") as f:
+    f.write("\n\n\n".join(sources))
